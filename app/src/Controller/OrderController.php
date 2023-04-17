fc5f4fc8be2ed25a939711eb6a341b2e56dcb9d9
Thomas Deschodt <thomas.deschodt@epitech.eu>
2023-04-17 11:37:59 +0200

    [+]Route Order

---
+++ app/src/Controller/OrderController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Order;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+use DateTime;
+use PhpParser\JsonDecoder;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Utils\Json;
+use App\Utils\Validate;
+
+class OrderController extends AbstractController
+{
+    #[Route('/orders', name: 'app_order_read_all', methods:['GET'])]
+    public function readAll(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $order = $entityManager->getRepository(Order::class)->findAll();
+        $arrayorders = array_map(function ($order) {return $order->getJson();}, $order);
+
+        return $this->json([
+            "message" => $arrayorders
+        ]);
+    }
+
+    #[Route('/orders/{orderId}', name: 'app_order_read_id',requirements: ['id'=>Requirement::DIGITS], methods:['GET'])]
+    public function read(EntityManagerInterface $entityManager, $orderId): JsonResponse
+    {
+        if (!is_numeric($orderId))
+            return $this->json([    
+                'error' => 'Product id must be a number']);
+
+        $order = $entityManager->getRepository(Order::class )->findById($orderId);
+
+        if(count($order)===0)
+            return $this->json([
+                'error' => 'Product not found']);
+       
+        return $this->json([
+           'product' => $order[0]->getJson()
+        ]);
+    }
+}
---
+++ app/src/Entity/Order.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 5)]
+    #[Assert\NotBlank]
+    private ?int $totalPrice = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private ?string $creationDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTotalPrice(): ?string
+    {
+        return $this->totalPrice;
+    }
+
+    public function setTotalPrice(string $totalPrice): self
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?string
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(string $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getJson(): array
+    {
+        return [
+            'id' => $this->getId(),
+            'totalPrice' => $this->getTotalPrice(),
+            'creationDate' => $this->getCreationDate(),
+        ];
+    }
+}
